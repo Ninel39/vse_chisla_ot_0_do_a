f30a4f2727c08e5667bada5982c8c4f080efd01f
User <User@WIN-5U3F60ENH1L>
2022-10-23 00:55:24 +0300

    Добавьте файлы проекта.

---
+++ vse_chisla_ot_0_do_a/vse_chisla_ot_0_do_a.cpp
@@ -0,0 +1,24 @@
+﻿// vse_chisla_ot_0_do_a.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int a;
+    cin >> a;
+    for (int i = 0; i <= a; i++)
+    {
+        cout << i << endl;
+    }
+
+
+
+}
+
+
+    
+
+
